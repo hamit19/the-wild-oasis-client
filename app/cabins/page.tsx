7efa773c9521e91b2de8da4f76bf3f4ceb3525e3
Hamid-Hassani <hamidhassani119@gmail.com>
2025-12-29 12:21:06 +0330

    fix/ orgonize the projects structure

---
+++ app/cabins/page.tsx
@@ -0,0 +1,20 @@
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  console.log(data);
+
+  return (
+    <div>
+      {" "}
+      cabins Page
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
