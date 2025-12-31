df24f9ab948e0e6f440e9dd8c27ea3729e49817e
Hamid-Hassani <hamidhassani119@gmail.com>
2025-12-31 12:43:31 +0330

    feat/ Implement home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
       <body
-        className={` ${josefinSans.className} text-gray-100 bg-primary-950 min-h-screen flex flex-col`}>
+        className={` ${josefinSans.className} relative text-gray-100 bg-primary-950 min-h-screen flex flex-col`}>
         <Header />
         <div className='flex-1 px-8 py-12'>
           <main className='max-w-7xl mx-auto'>{children}</main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import bg from "@/public/bg.png";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>The Wild Oasis. Welcome to paradise!</h1>
-    </div>
+    <main className='mt-24'>
+      <Image
+        src={bg}
+        fill
+        className='object-cover object-top'
+        alt='Mountains and forests with two cabins'
+        placeholder='blur'
+      />
+      <div className='text-center relative z-10'>
+        <h1 className='text-8xl text-center text-primary-50 font-normal mb-12'>
+          Welcome to Paradise.
+        </h1>
+
+        <Link
+          className='bg-accent-500 text-primary-800 text-xl font-semibold px-8 py-6 hover:bg-accent-600 transition-all rounded-sm'
+          href={"/cabins"}>
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
   );
 }
