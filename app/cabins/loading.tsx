070c166a7a8c0423857b7520353685910bb69f0b
Hamid-Hassani <hamidhassani119@gmail.com>
2026-01-05 15:12:49 +0330

    feat: add loading spinner for cabins page and a global loading spinner

--- app/cabins/loading.tsx
+++ app/cabins/loading.tsx
@@ -1,3 +1,12 @@
+import Spinner from "../_components/Spinner";
+
 export default function Loading() {
-  return <div>cabins loading...</div>;
+  return (
+    <div className='grid items-center justify-center'>
+      <Spinner />
+      <div className=' text-xl text-gray-200'>
+        <span>Loading cabins data...</span>
+      </div>
+    </div>
+  );
 }
---
+++ app/loading.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Spinner from "./_components/Spinner";
+
+export default function Loading() {
+  return <Spinner />;
+}
