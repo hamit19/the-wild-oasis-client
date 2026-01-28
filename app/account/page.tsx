f9e4703af655698e3ff441c4560b3d67b88c11dc
Hamid-Hassani <hamidhassani119@gmail.com>
2026-01-28 16:16:42 +0330

    feat: add users name to the account info page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,18 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Account",
 };
 
-export default function Page() {
-  return <div> Welcome Hamid!</div>;
+export default async function Page() {
+  const session = await auth();
+
+  const username = session?.user?.name?.split(" ").at(0);
+
+  return (
+    <span className='text-accent-400 font-semibold text-lg '>
+      {" "}
+      Welcome, {username}
+    </span>
+  );
 }
