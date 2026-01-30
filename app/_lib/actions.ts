708ce35c05bc679000b1d4256ebfd383d682ac6d
Hamid Hassani <hamidhassani119@gmail.com>
2026-01-30 14:35:44 +0330

    feat: add updateGuest method

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
 
 export async function signInAction() {
   return await signIn("google", { redirectTo: "/account" });
@@ -8,4 +9,36 @@ export async function signInAction() {
 
 export async function signOutAction() {
   return await signOut({ redirectTo: "/" });
+}
+
+export default async function updateGuest(formData: FormData) {
+  const session = await auth();
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData?.get("nationality")?.split("%");
+
+  if (nationalID && nationality && countryFlag) {
+    const updateGuest = { nationalID, nationality, countryFlag };
+
+    if (!session) throw new Error("Please login first!");
+
+    if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+      throw new Error("invalid nationalID!");
+
+    const { data, error } = await supabase
+      .from("guests")
+      .update(updateGuest)
+      .eq("id", session?.user?.id)
+      .select()
+      .single();
+
+    if (error) {
+      console.error(error);
+      throw new Error("Guest could not be updated");
+    }
+
+    return data;
+  } else {
+    throw new Error("Please fill out all of the fields!");
+  }
 }
--- app/_types/filterTypes.ts
+++ app/_types/filterTypes.ts
@@ -1,1 +1,9 @@
 export type filterType = "small" | "medium" | "large" | "all";
+export type guestType = {
+    guestId: string,
+    nationality: string,
+    nationalID: string,
+    countryFlag: string,
+    fullName: string,
+    email:string,
+}
