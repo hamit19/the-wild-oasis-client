1c0def45faac073834aad0675827cf618e1f4088
Hamid Hassani <hamidhassani119@gmail.com>
2026-02-03 18:33:14 +0330

    fix: the children props error in textExpander compo

--- app/_components/Cabin.tsx
+++ app/_components/Cabin.tsx
@@ -21,7 +21,7 @@ export default function ({ cabin }: { cabin: Cabin }) {
         </h3>
 
         <p className="text-lg text-primary-300 mb-10">
-          <TextExpander>{cabin.description}</TextExpander>
+          <TextExpander text={cabin.description} />
         </p>
 
         <ul className="flex flex-col gap-4 mb-7">
